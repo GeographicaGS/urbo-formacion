2f366c852265554d0372a7af89093360aaae6701
Roberto García Calero <rodanber@gmail.com>
2018-08-23 11:53:08 +0200

    fix: Add missing script to bootstrap.sql

--- students/api/db/bootstrap.sql
+++ students/api/db/bootstrap.sql
@@ -4,4 +4,9 @@
 
 -- DDL
 \ir common/ddl/urbo_createtables_students.sql
+
+-- DML
 \ir common/dml/urbo_createmetadata_students.sql
+
+-- Processing functions
+\ir common/urbo_students_calculate_distances.sql
